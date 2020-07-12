e7e7183b6582f4d1ec09e6e63c74ed22ac6db3a6
zhangle <zhangle@vtstar.net>
2020-07-12 15:09:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <right-panel>
+      <div>hello</div>
+    </right-panel>
+    <!--<iframe id="iframe" src="http://localhost:8080/#/form-code" frameborder="0"></iframe>-->
+  </div>
+</template>
+
+<script>
+import RightPanel from './components/RightPanel/index.vue'
+export default {
+  name: 'App',
+  components:{
+    RightPanel
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+#iframe{
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  width: 90%;
+  height: 90%;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI);
+
+const root = document.createElement('div') //创建div节点
+document.body.appendChild(root) //将div节点添加到body下
+
+new Vue({
+  render: (h) => h(App), //vue在创建Vue实例时,通过调用render方法来渲染实例的DOM树,也就是这个组件渲染的是App的内容
+  //vue在调用render方法时,会传入一个createElement函数作为参数,也就是这里的h的实参是createElement函数,然后createElement会以App为参数进行调用
+}).$mount(root)
